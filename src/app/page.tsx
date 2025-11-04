880f05d7d2b7ddb0ecbd350faee35a0fcf961400
MayorChristopher <mayoru24@gmail.com>
2025-11-04 18:00:01 +0100

    Improve scroll animations: more movement, better easing, earlier trigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           }
         })
       },
-      { threshold: 0.1 }
+      { threshold: 0.15, rootMargin: '0px 0px -100px 0px' }
     )
 
     document.querySelectorAll('.fade-in').forEach((el) => observer.observe(el))
